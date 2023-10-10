4bf4b755138a348d9150ad7d7cfa7552f179bca8
Tanishqa Arora <tanishqa164@gmail.com>
2023-10-10 23:40:56 +0530

    Initialize project using Create React App

---
+++ countries.js
@@ -0,0 +1,7 @@
+export const countries = [
+    { id: 1, name: 'India'},
+    { id: 2, name: 'USA' },
+    { id: 3, name: 'Canada' },
+];
+
+  
---
+++ states.js
@@ -0,0 +1,6 @@
+export const states = {
+    1: ['Punjab', 'Uttar Pradesh', ]
+    2: ['New York', 'California', 'Texas'],
+    3: ['Ontario', 'Quebec', 'British Columbia'],
+  };
+  
